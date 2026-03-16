092df779c9cba3ce79f7f8b52282c3b85b9aa1fe
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2026-03-16 09:11:03 +0530

    some update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
- 
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -12,11 +10,11 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-  
+
